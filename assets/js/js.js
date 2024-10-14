ff492fd1d280bab89c38ad23ea294771a621ce3a
raimer aguilar <aguilar.raimer@gmail.com>
2024-10-14 01:36:26 -0300

    segundo commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,8 +5,11 @@ const ListaId = document.querySelector("#lista-id")
 const ListaTareas = document.querySelector("#lista-tareas")
 const totalTareas = document.querySelector("#total")
 const totalRealizadas = document.querySelector("#realizadas")
-const tareas= []
 var contadorRealizadas = 0
+const tareas= [ { id: 1, nombre: "Tarea 1", completado: true },
+                { id: 2, nombre: "Tarea 2", completado: false },
+                { id: 3, nombre: "Tarea 3", completado: true }]
+
 
 agregar = () =>{
     let agregarTarea = {id: Date.now(), nombre: Tareainput.value, completado: false} 
@@ -19,6 +22,7 @@ agregar = () =>{
     input.value=""
     render()
 }
+
 render = () => {
     let elementoUno = ""
     let elementoDos = "" 
@@ -26,45 +30,50 @@ render = () => {
     for(let tarea of tareas){
         elementoUno+= `<li>${tarea.id}</li>`
         elementoDos+= `<li>${tarea.nombre} 
-        <input type="checkbox" id="${tarea.id}" class="checkbox" onchange="checkStatus(${tarea.id}) " />
+        <input type="checkbox" id="${tarea.id}" class="checkbox" onclick="checkStatus(${tarea.id}) " />
         <button class="btn-eliminar" onclick="eliminar(${tarea.id})">X</button></li> `
-
     }
+
     ListaId.innerHTML= elementoUno
     ListaTareas.innerHTML= elementoDos
-    
+
+    const contadorObjetos = tareas.length;
+    totalTareas.innerHTML = `Total Pendientes:${contadorObjetos}`;
 }
 
  checkStatus= (e) => {
-    const checkbox = document.getElementById(e);
+    let checkbox = document.getElementById(e);
     if(checkbox.checked) {
         tareas.completado = true   
       }else 
       { tareas.completado = false   
     }
-    if(tareas.completado ===true){
+    if(tareas.completado === true){  
         contadorRealizadas++
         totalRealizadas.innerHTML = `Realizadas:${contadorRealizadas}`
     }else if(tareas.completado ===false){
         contadorRealizadas--
         totalRealizadas.innerHTML = `Realizadas:${contadorRealizadas}`
     }
-
 }
 
 
 eliminar = (id) =>{
     const index = tareas.findIndex((e) => e.id == id)
-    tareas.splice(index, 1)
-    contadorTareas()
-    render()
-}
 
-        
-contadorTareas = () => {
-    const contadorObjetos = tareas.length;
-    totalTareas.innerHTML = `Total Pendientes:${contadorObjetos}`;
+    if(tareas.completado === true){
+        contadorRealizadas--
+        totalRealizadas.innerHTML = `Realizadas:${contadorRealizadas}`
+        tareas.splice(index, 1)
+    }else{
+        tareas.splice(index, 1)
+    }
+
+    render()
+
+
 }
+render()
 
 btnAgregar.addEventListener("click", () =>{
    agregar()
